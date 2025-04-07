b26b42d7f443f3a0673da4f30b445c6b048f2cf0
yashdubeyw3 <yash.dubey@w3villa.com>
2025-04-07 16:52:16 +0530

    updated

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
 const launchedDate = new Date("2025-4-12").getTime();
+
 function clock() {
   const currentDate = new Date().getTime();
   const gap = launchedDate - currentDate;
   console.log(gap);
 
-  const days = Math.floor(gap / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((gap % (1000 * 60)) / 1000);
+  const d = Math.floor(gap / (1000 * 60 * 60 * 24));
+  const hr = Math.floor((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const min = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+  const sec = Math.floor((gap % (1000 * 60)) / 1000);
   
-  document.getElementById("days").textContent = days;
-  document.getElementById("hours").textContent = hours;
-  document.getElementById("minutes").textContent = minutes;
-  document.getElementById("seconds").textContent = seconds;
+  days.textContent = d;
+  hours.textContent = hr;
+  minutes.textContent = min;
+  seconds.textContent = sec;
 }
 setInterval(clock, 1000);
 
